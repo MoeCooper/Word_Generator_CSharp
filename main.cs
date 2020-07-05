5a4c64756032c0f7f542f35b36ca6290b66ffa82
Maurice <jc70240@gmail.com>
2020-07-05 03:55:40 +0000

    Working on more logic

--- main.cs
+++ main.cs
@@ -4,7 +4,8 @@ using System.IO;
 
 class MainClass 
 {
-    public static void Main (string[] args) 
+
+    static void MenuChoice(string UserInput)
     {
         Console.WriteLine("********MENU********");
         Console.WriteLine("1) Random Word");
@@ -12,31 +13,44 @@ class MainClass
         Console.WriteLine("3) Random Verb");
         Console.WriteLine("4) Random Adjective");
         Console.WriteLine("9) EXIT PROGRAM");
+    }
+
+
+    public static void Main (string[] args) 
+    {
 
         string UserInput = Console.ReadLine();
-        Console.WriteLine(UserInput);
-
         string caseSwitch = UserInput;
-        switch(caseSwitch) 
+
+        while (UserInput !="9")
         {
-            case "1":
-                Console.WriteLine("This will get a random word from ALL WORDS.");
-                break;
-            case "2":
-                Console.WriteLine("This will get a random word from NOUNS.");
-                break;
-            case "3":
-                Console.WriteLine("This will get a random word from VERBS.");
-                break;
-            case "4":
-                Console.WriteLine("This will get a random word from ADJECTIVES.");
-                break;
-            case "9":
-                Environment.Exit(0);
-                break;
-            default:
-                Console.WriteLine("Unknown Choice. Please choose 1, 2, 3, 4, or type 9 to exit.");
-                break;
-       }   
+            switch(caseSwitch) 
+            {
+                case "1":
+                    Console.WriteLine("This will get a random word from ALL WORDS.");
+                    MenuChoice(UserInput);
+                    break;
+                case "2":
+                    Console.WriteLine("This will get a random word from NOUNS.");
+                    MenuChoice(UserInput);
+                    break;
+                case "3":
+                    Console.WriteLine("This will get a random word from VERBS.");
+                    MenuChoice(UserInput);
+                    break;
+                case "4":
+                    Console.WriteLine("This will get a random word from ADJECTIVES.");
+                    MenuChoice(UserInput);
+                    break;
+                case "9":
+                    Console.WriteLine("Exiting program.... Goodbye!");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Unknown Choice. Please choose 1, 2, 3, 4, or type 9 to exit.");
+                    MenuChoice(UserInput);
+                    break;
+            }
+        }  
     }
 }
